0ab750a8ebb377afe4d796c5d0b776595d0aaa09
Adiyam Nerri <adiyam@gmail.com>
2017-08-22 02:51:57 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,18 +1,19 @@
 var chocolateBars = ['snickers', 'hundred grand', 'kitkat', 'skittles'];
 
-function addElementToBeginningOfArray(array, elements) {
-  return [elements, ...array ];
+function addElementToBeginningOfArray(array, element) {
+  return [element, ...array ];
 }
 
-function destructivelyAddElementToBeginningOfArray(array3, elements2) {
-  array3.unshift(elements2);
-  return array3;
+function destructivelyAddElementToBeginningOfArray(array, elements) {
+  array.unshift(element);
+  return array;
 }
 
-function addElementToEndOfArray(array2, elements4) {
-  return
+function addElementToEndOfArray(array, element) {
+  return [...array, element];
 }
 
-function destructivelyAddElementToEndOfArray(array2, elements4) {
-  return array2 = [array2, elements4];
+function destructivelyAddElementToEndOfArray(array, element) {
+  array.push(element);
+  return array;
 }
