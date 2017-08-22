cbb766466aa72b0c93d426cf49fae13b10a87361
Adiyam Nerri <adiyam@gmail.com>
2017-08-22 03:00:13 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -21,3 +21,8 @@ function destructivelyAddElementToEndOfArray(array, element) {
 function accessElementInArray(array, element) {
   return array[element];
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift();
+  return array;
+}
